4fcd60c3ff2d14c39aa6158392fb46728ad12156
Michael Alexander <beefsack@gmail.com>
2009-07-28 10:09:02 +0000

    Played with test controller

--- tourney/application/controllers/TestController.php
+++ tourney/application/controllers/TestController.php
@@ -7,25 +7,6 @@ class TestController extends Zend_Controller_Action
 	{
 	}
 	
-	public function playersAction()
-	{
-		$writer = new Zend_Log_Writer_Stream(APPLICATION_PATH . '/log/log.txt');
-		$logger = new Zend_Log($writer);
-		$logger->log('players called', 7);
-		if ('ajax' != $this->_getParam('format', false)) {
-			$logger->log('format not set to ajax, exiting', 7);
-			return $this->_helper->redirector('index');
-		}
-		$logger->log('format set to ajax, returning json object', 7);
-		$table = new Model_DbTable_User();
-		$result = $table->fetchAll();
-		$data = array();
-		foreach ($result as $row) {
-			$data[] = $row['name'];
-		}
-		$this->_helper->autoCompleteDojo($data);
-	}
-
 	public function tourneylistAction()
 	{
 		Zend_Debug::dump(Model_Type_Abstract::getTypeList());
@@ -53,81 +34,38 @@ class TestController extends Zend_Controller_Action
 		$post = $this->getRequest()->getPost();
 		// This action will create a tournament and then save it to the database
 		$eggtourney = new Model_Type_SingleElimination(); // Make a new ladder tournament
-		// Make a new type of game, and save it to the database to make it ready
-		// Here we are creating and saving a new game type, but to load a game from the database say with the id of 1, you would do:
-		// $game = new Model_Game(1);
-		// OR
-		// $game = new Model_Game();
-		// $game->load(1);
-		$game = new Model_Game();
-		$game->setName("Test Game");
-		$game->setDescription("This is a new type of game");
-		$game->setScoringtype(new Model_VictoryCondition_HighestScore());
-		$game->save(); // Will throw an exception if any one of name, description or scoringtype isn't set
-		$eggtourney->setGame($game); // Set the game type of the tourney to this new game.  It's also possible to pass an integer to setGame equivalent to the game id in the database
-		// Now we will set the matchup type for the tournament
-		if (isset($post['typesubform']['matchuptype'])) {
+		if ($post) {
+			// Make a new type of game, and save it to the database to make it ready
+			// Here we are creating and saving a new game type, but to load a game from the database say with the id of 1, you would do:
+			// $game = new Model_Game(1);
+			// OR
+			// $game = new Model_Game();
+			// $game->load(1);
+			$game = new Model_Game();
+			$game->setName("Test Game");
+			$game->setDescription("This is a new type of game");
+			$game->setScoringtype(new Model_VictoryCondition_HighestScore());
+			$game->save(); // Will throw an exception if any one of name, description or scoringtype isn't set
+			$eggtourney->setGame($game); // Set the game type of the tourney to this new game.  It's also possible to pass an integer to setGame equivalent to the game id in the database
+			// Now we will set the matchup type for the tournament
 			$eggtourney->setMatchuptype(new $post['typesubform']['matchuptype']);
-		} else {
-			$eggtourney->setMatchuptype(new Model_MatchupType_Random());
-		}
-		// Now make a list of participants for the tourney
-		$participantlist = new Model_ParticipantList(); // Create a participant list so we can add participants to it.  A participant list is a fancy array.
-		// You can make a participant out of anything!  As long as it implements Model_Participantable (lol)
-		// Putting a string when creating a Model_User will load that user from the database.  If that model isn't there, it will throw an exception.
-		if (isset($post['player'])) {
+			// Now make a list of participants for the tourney
+			$participantlist = new Model_ParticipantList(); // Create a participant list so we can add participants to it.  A participant list is a fancy array.
+			// You can make a participant out of anything!  As long as it implements Model_Participantable (lol)
+			// Putting a string when creating a Model_User will load that user from the database.  If that model isn't there, it will throw an exception.
 			foreach ($post['player'] as $player) {
 				$participant = new Model_Participant();
 				$user = new Model_User($player);
 				$participant->set($user);
 				$eggtourney->addParticipant($participant);
 			}
-		} else {
-			$participant = new Model_Participant();
-			$user = new Model_User('beefsack');
-			$participant->set($user);
-			$eggtourney->addParticipant($participant);
-			// Lets add one more
-			$participant = new Model_Participant();
-			$user = new Model_User('baconheist');
-			$participant->set($user);
-			$eggtourney->addParticipant($participant);
-			// Lets add one more
-			$participant = new Model_Participant();
-			$user = new Model_User('test1');
-			$participant->set($user);
-			$eggtourney->addParticipant($participant);
-			// Lets add one more
-			$participant = new Model_Participant();
-			$user = new Model_User('test2');
-			$participant->set($user);
-			$eggtourney->addParticipant($participant);
-			// Lets add one more
-			$participant = new Model_Participant();
-			$user = new Model_User('test3');
-			$participant->set($user);
-			$eggtourney->addParticipant($participant);
-			// Lets add one more
-			$participant = new Model_Participant();
-			$user = new Model_User('test4');
-			$participant->set($user);
-			$eggtourney->addParticipant($participant);
-			// Lets add one more
-			$participant = new Model_Participant();
-			$user = new Model_User('test5');
-			$participant->set($user);
-			$eggtourney->addParticipant($participant);
-			// Lets add one more
-			$participant = new Model_Participant();
-			$user = new Model_User('test6');
-			$participant->set($user);
-			$eggtourney->addParticipant($participant);
-		}
-		// Now we have a list of participants, a game, and a matchup type, lets save the tourney which will build it and save it to the database.
-		$eggtourney->save();
-		 
-		$this->view->tourneyTree = $eggtourney->getTree();
+			// Now we have a list of participants, a game, and a matchup type, lets save the tourney which will build it and save it to the database.
+			$eggtourney->save();
+
+			$this->view->tourneyTree = $eggtourney->getTree();
 		
+		}
+		 
 		$this->view->headScript()->appendFile(PUBLIC_PATH . '/js/addPlayer.js');
 		
 		$form = $eggtourney->getForm();
