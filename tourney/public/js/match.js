8b9ed4c041da92a9d083795878ae960294358fb8
Michael Alexander <beefsack@gmail.com>
2009-08-04 03:58:44 +0000

    Enabled form validation

--- tourney/public/js/match.js
+++ tourney/public/js/match.js
@@ -8,24 +8,27 @@ function getMatchForm(matchid)
 
 function saveMatchForm()
 {
-	// Sends data to a URL using POST, there is also a form xhrGet to send using GET
-	dojo.xhrPost({
-		// The url to submit the form data to
-		url: "/tourney/public/ajax/savematchscore",
-		// The form to submit
-		form: dojo.byId("scoreinputform"),
-		// It is just a text form (ie. no file uploads)
-		handleAs: "text",
-		// What to do once the page has been loaded (ie. the form was successfully sent)
-		load: function(data) {
-			// Reload the tree and hide the dialog
-			dijit.byId('treecontentpane').attr('href', '/tourney/public/ajax/tree/tourneyid/' + document.getElementById('tourneyid').value);
-			dijit.byId('scoredialog').hide();
-		},
-		// What to do if there is an error
-		error: function(error) {
-			alert('Error saving match');
-		}
-	});
+	// Only try to save if the form is valid
+	if (dijit.byId("scoreinputform").isValid()) {
+		// Sends data to a URL using POST, there is also a form xhrGet to send using GET
+		dojo.xhrPost({
+			// The url to submit the form data to
+			url: "/tourney/public/ajax/savematchscore",
+			// The form to submit
+			form: dojo.byId("scoreinputform"),
+			// It is just a text form (ie. no file uploads)
+			handleAs: "text",
+			// What to do once the page has been loaded (ie. the form was successfully sent)
+			load: function(data) {
+				// Reload the tree and hide the dialog
+				dijit.byId('treecontentpane').attr('href', '/tourney/public/ajax/tree/tourneyid/' + document.getElementById('tourneyid').value);
+				dijit.byId('scoredialog').hide();
+			},
+			// What to do if there is an error
+			error: function(error) {
+				alert('Error saving match');
+			}
+		});
+	}
 	return false; // This stops the form from actually submitting and opening a new page.  We want to stay on the same page and let ajax reload element
 }
